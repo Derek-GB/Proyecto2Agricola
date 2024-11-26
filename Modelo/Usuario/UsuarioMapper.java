6500b369ddadf7d71cb7f27adb61ad86c0f46048
Fernando.Helio <febolivarbr@est.utn.ac.cr>
2024-11-25 22:03:08 -0600

    UsuarioMapper
    Se creo el Mapper para la Clase Usuario

---
+++ Modelo/Usuario/UsuarioMapper.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.Usuario;
+
+import Enums.Rol;
+import Modelo.Mapper.Mapper;
+
+/**
+ *
+ * @author Fernando
+ */
+public class UsuarioMapper implements Mapper<Usuario, UsuarioDTO> {
+
+    @Override
+    public UsuarioDTO toDTO(Usuario ent) {
+        String rol = ent.getRol().name();
+        return new UsuarioDTO(ent.getId(), ent.getNombre(), ent.getContraseña(), rol);
+    }
+
+    @Override
+    public Usuario toEnt(UsuarioDTO dto) {
+        Rol rol = Rol.valueOf(dto.getRol());
+        return new Usuario(dto.getId(), dto.getNombre(), dto.getContraseña(), rol);
+    }
+
+}
