294550ea0080ad08b6f7b8951b08bcf1a9d4e072
Tony <anconejoro@est.utn.ac.cr>
2024-11-27 12:46:31 -0600

    se actualizo la clase DAO
    no hubo grandes cambios solo produccionDAO falta mapper

--- Modelo/Produccion/ProduccionDAO.java
+++ Modelo/Produccion/ProduccionDAO.java
@@ -5,39 +5,112 @@
 package Modelo.Produccion;
 
 import Modelo.DAO.Dao;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 /**
  *
  * @author Tony
  */
-//public class ProduccionDAO  extends Dao<ProduccionDTO> {
-//
-//    @Override
-//    public boolean create(ProduccionDTO dto) throws SQLException {
-//      
-//    }
-//
-//    @Override
-//    public ProduccionDTO read(Object id) throws SQLException {
-//    
-//    }
-//
-//    @Override
-//    public List<ProduccionDTO> readAll() throws SQLException {
-//    
-//    }
-//
-//    @Override
-//    public boolean update(ProduccionDTO dto) throws SQLException {
-//    
-//    }
-//
-//    @Override
-//    public boolean delete(Object id) throws SQLException {
-//       
-//        
-//    }
-//    
-//}
+public class ProduccionDAO  extends Dao<ProduccionDTO> {
+
+    public ProduccionDAO(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public boolean create(ProduccionDTO dto) throws SQLException {
+      if (dto == null || !validatePK(dto.getId())) {
+            return false;
+        }
+
+        String query = "Call ProduccionCreate(?,?,?,?,?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, dto.getId());
+            stmt.setDate(2, dto.getFecha());
+            stmt.setDouble(3, dto.getCantidadRecolectada());
+            stmt.setString(4, dto.getCalidad());
+            stmt.setString(5, dto.getDestino());
+            
+            return stmt.executeUpdate() > 0;
+        }
+    }
+
+    @Override
+    public ProduccionDTO read(Object id) throws SQLException {
+      if (id == null || String.valueOf(id).trim().isEmpty()) {
+            return null;
+        }
+        String query = "Call ProduccionRead(?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setString(1, String.valueOf(id));
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new ProduccionDTO(rs.getInt(0)
+                            ,rs.getDate(0)
+                            , rs.getDouble(0)
+                            , rs.getString(0)
+                            , rs.getString(0));
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<ProduccionDTO> readAll() throws SQLException {
+      String query = "Call ProduccionReadAll()";
+        List<ProduccionDTO> list = new ArrayList<>();
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                   list.add(new ProduccionDTO(rs.getInt(1)
+                            ,rs.getDate(2)
+                            , rs.getDouble(3)
+                            , rs.getString(4)
+                            , rs.getString(5)));
+                }
+            }
+        }
+        return list;
+        
+    }
+
+    @Override
+    public boolean update(ProduccionDTO dto) throws SQLException {
+    if (dto == null) {
+            return false;
+        }
+
+        String query = "Call ProduccionUpdate(?,?,?,?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+           stmt.setInt(1, dto.getId());
+           stmt.setDouble(2, dto.getCantidadRecolectada());
+           stmt.setString(3, dto.getCalidad());
+           stmt.setString(4, dto.getDestino());
+            return stmt.executeUpdate() > 0;
+        }
+    }
+
+    @Override
+    public boolean delete(Object id) throws SQLException {
+              if (id == null || String.valueOf(id).trim().isEmpty()) {
+            return false;
+        }
+        String query = "Call ProduccionDelete(?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setString(1, String.valueOf(id));
+            return stmt.executeUpdate() > 0;
+        }
+    }
+
+    public boolean validatePK(Object id) throws SQLException {
+        return read(id) == null;
+    }
+        
+    }
+    
